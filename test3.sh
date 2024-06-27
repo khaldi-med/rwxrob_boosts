1635725ba2de07979bf7f45fadfd3e7185f02c79
mohamed <khaldimohamed82@gmail.com>
2024-06-27 23:10:31 +0100

    My learning journey practice

--- test3.sh
+++ test3.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-var="nef892na9s1p9asn2aJs71nIsm"
-
-for counter in {1..40}
-do
-	var=$(echo $var | base64)
-	if [ $counter -eq 35 ]
-	then
-		echo $var | wc -c
-		exit 0
-	else
-		echo $counter
-	fi
-done
+FILELIST=`ls`
+FileWithTimeStamp=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
